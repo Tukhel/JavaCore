21b2e7054f17ec0b0202cb1216c98de73c8c8a01
Tukhel <Andrew_melf@mail.ru>
2019-04-09 14:53:31 +1000

    1. Массив строк. Список уникальных слов из массива. Список с количество повторений каждого в массиве. 2. Телефонная книга, с возможность добавления телефон-фамилия и поиска по фамилии.

--- src/ru/geekbrains/lesson3/Main.java
+++ src/ru/geekbrains/lesson3/Main.java
@@ -1,4 +1,47 @@
 package ru.geekbrains.lesson3;
 
+import java.util.*;
+
 public class Main {
+    public static void main(String[] args) {
+        String[] animal = {"Собака", "Кошка", "Воробей",
+                "Дельфин", "Воробей", "Пантера",
+                "Кошка", "Лось", "Тигр", "Сова" ,
+                "Дельфин", "Воробей" , "Енот"};
+
+        System.out.println("Массив: " + Arrays.toString(animal));
+
+        LinkedHashSet<String> uniqueAnimal = new LinkedHashSet<>();
+        for (int i = 0; i < animal.length; i++){
+            uniqueAnimal.add(animal[i]);
+        }
+
+        System.out.println("Уникальные слова из массива: " + uniqueAnimal);
+
+        Map<String, Integer> numberOfRepeat = new HashMap<>();
+        for (int i = 0; i < animal.length; i++) {
+            int count = 0;
+            for (int j = 0; j < animal.length; j++) {
+                if (animal[i] == animal[j]) {
+                    count++;
+                    numberOfRepeat.put(animal[i], count);
+                }
+            }
+        }
+        System.out.println("Колличество повторений слов в массиве: " + numberOfRepeat);
+
+        Phonebook phonebook = new Phonebook();
+
+        phonebook.add(2302500, "Иванов");
+        phonebook.add(2501501, "Иванов");
+        phonebook.add(2747474, "Петров");
+        phonebook.add(2123456, "Петров");
+        phonebook.add(2987654, "Сидоров");
+        phonebook.add(2222222, "Гольдман");
+
+        phonebook.get("Иванов");
+        phonebook.get("Петров");
+        phonebook.get("Сидоров");
+        phonebook.get("Энштейн");
+    }
 }
--- src/ru/geekbrains/lesson3/PhoneBook.java
+++ src/ru/geekbrains/lesson3/PhoneBook.java
@@ -1,4 +1,25 @@
 package ru.geekbrains.lesson3;
 
-public class PhoneBook {
+import java.util.*;
+
+public class Phonebook {
+
+    Map<Integer, String> phonebook = new HashMap<>();
+
+    public void add(int number, String surname) {
+        phonebook.put(number, surname);
+    }
+
+    public void get(String surname) {
+        if (phonebook.containsValue(surname)) {
+            Set<Map.Entry<Integer, String>> set = phonebook.entrySet();
+            for (Map.Entry<Integer, String> temp : set) {
+                if (temp.getValue().equals(surname)) {
+                    System.out.println(temp.getValue() + " : " + temp.getKey());
+                }
+            }
+        } else {
+            System.out.printf("Фамилия %s в справочнике не найдена", surname);
+        }
+    }
 }
