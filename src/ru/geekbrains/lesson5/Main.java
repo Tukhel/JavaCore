ce013b051e405bfdef3003edd9f3a86a1a2a8f7b
Tukhel <Andrew_melf@mail.ru>
2019-04-16 21:54:48 +1000

    ДЗ5

---
+++ src/ru/geekbrains/lesson5/Main.java
@@ -0,0 +1,80 @@
+package ru.geekbrains.lesson5;
+
+public class Main {
+
+    static final int SIZE = 10000000;
+    static final int H = SIZE /2;
+
+    public static void main(String[] args) {
+        Main e1 = new Main();
+        e1.method1();
+        e1.method2();
+    }
+
+    public void method1 () {
+
+        float[] arr = new float[SIZE];
+
+        for(int i = 0; i<arr.length; i++) {
+            arr[i]=1;
+        }
+
+        long startTime = System.currentTimeMillis();
+        for(int i = 0; i<arr.length; i++) {
+            arr[i]=(float)(arr[i] * Math.sin(0.2f+ i/5)* Math.cos(0.2f+ i/5) * Math.cos(0.4f + i/2));
+        }
+        long finishtime = System.currentTimeMillis();
+
+        System.out.println("Время выполнения метода 1 : " + (finishtime - startTime));
+
+    }
+
+    public void method2 () {
+
+        float[] arr = new float[SIZE];
+        float[] a1 = new float[H];
+        float[] a2 = new float[H];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 1;
+        }
+
+        long startTime = System.currentTimeMillis();
+        System.arraycopy(arr,0,a1,0, H);
+        System.arraycopy(arr, H,a2,0, H);
+
+        Thread cal1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 0; i<a1.length; i++) {
+                    a1[i]=(float)(a1[i] * Math.sin(0.2f+ i/5)* Math.cos(0.2f+ i/5) * Math.cos(0.4f + i/2));
+                }
+            }
+        });
+        cal1.start();
+
+        Thread cal2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 0; i<a2.length; i++) {
+                    a2[i]=(float)(a2[i] * Math.sin(0.2f+ i/5)* Math.cos(0.2f+ i/5) * Math.cos(0.4f + i/2));
+                }
+            }
+        });
+        cal2.start();
+
+        try {
+            cal1.join();
+            cal2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.arraycopy(a1, 0, arr, 0, H);
+        System.arraycopy(a2, 0, arr, H, H);
+
+        long finishtime = System.currentTimeMillis();
+
+        System.out.println("Время выполнения метода 2 : " + (finishtime - startTime));
+    }
+}
